58104e835f2fab1eb95317c0cea93820282aa3b4
Amritanshu323 <70333706+Amritanshu323@users.noreply.github.com>
2023-12-11 10:14:54 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+const apiKey = "" used incase of youtube apiKey
+const videoId = "" used incase of youtube id 
+const tag = document.createElement('script');
+const firstScriptTag = document.getElementsByTagName('script');
+let player;
